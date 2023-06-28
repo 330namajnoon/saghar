8a8b4205a6791112f1cb02a9ea97a7cbc1c025e6
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2023-06-28 19:06:16 +0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.static(pdp));
 const server = http.createServer(app);
+const fileUploader = multer({storage:multer.diskStorage({
+    destination:(req,file,cd)=> {
+        cd(null,"../client/public/mediya");
+    },
+    filename:(req,file,cd)=> {
+        cd(null,file.originalname);
+    }
+})})
 server.listen(port,()=> {
     console.log(`server is up on port ${port}!`);
 })
@@ -21,4 +29,25 @@ app.post("/appData",multer().none(),(req,res)=> {
             res.send(JSON.parse(data.toString()).find(u => u.id == req.body.id));
         }
     })
+})
+app.post("/updateData",multer().none(),(req,res)=> {
+    fs.readFile("./data.json",(err,data)=> {
+        if(!err) {
+            const users = JSON.parse(data.toString());
+            let index = users.indexOf(users.find(u => u.id == req.body.id));
+            users[index] = JSON.parse(req.body.data);
+            console.log(users[index])
+            fs.writeFile("./data.json",JSON.stringify(users),(err)=> {
+                if(!err) {
+                    res.send(users.find(u => u.id == req.body.id));
+                }
+            })
+        }
+    })
+})
+
+app.post("/uploadFile",fileUploader.single("file"),(req,res)=> {
+
+    fs.renameSync(`../client/public/mediya/${req.file.originalname}`,`../client/public/mediya/${req.body.id}.${req.file.originalname.split(".")[1]}`);
+    res.send(true);
 })
